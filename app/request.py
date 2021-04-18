64822bc5a430593c5eabf5b34e76c3e82467c237
Alice-Githui <gnyokabi.alice@gmail.com>
2021-04-18 14:23:34 +0300

    Add flask forms

--- app/request.py
+++ app/request.py
@@ -95,18 +95,18 @@ def get_sources():
 
         return final_results
 
-def search_news(news_title):
-    search_news_url = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'.format(news_title,apiKey)
+# def search_news(news_title):
+#     search_news_url = 'https://newsapi.org/v2/everything?q={}&language=en&apiKey={}'.format(news_title,apiKey)
 
-    with urllib.request.urlopen(search_news_url) as url:
-        search_news_data = url.read()
-        search_news_response =json.loads(search_news_data)
+#     with urllib.request.urlopen(search_news_url) as url:
+#         search_news_data = url.read()
+#         search_news_response =json.loads(search_news_data)
 
-        search_news_results = None
+#         search_news_results = None
 
-        if search_news_response['articles']:
-            search_news_list = search_news_response['articles']
-            search_movie_results = process_results(search_news_list)
+#         if search_news_response['articles']:
+#             search_news_list = search_news_response['articles']
+#             search_movie_results = process_results(search_news_list)
 
-    return search_news_results
+#     return search_news_results
 
