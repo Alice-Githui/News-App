0c98534d94475b4dd76d7dd1b93c51f67ccd5255
Alice-Githui <gnyokabi.alice@gmail.com>
2021-04-19 14:27:56 +0300

    Add more news categories to the landingpage

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect,url_for
 from .import main
-from ..request import get_sources, get_news
+from ..request import get_sources, get_news, search_news
 
 #Views/Routes to our apps
 @main.route('/')
@@ -19,15 +19,21 @@ def index():
 
     title = "Welcome to the Daily News App. Catch the day\'s news all in one place"
 
-    return render_template('index.html', title= title, sports=sports, headlines= headlines, technology=technology, general=general, beauty=beauty, entertainment= entertainment, trending=trending, sources=sources)
+    search_news = request.args.get('news_query')
 
-# @app.route('/search/<news_title>')
-# def search(news_title):
-#     '''
-#     View function to display the search results
-#     '''
-#     news_title_list = news_title.split(" ")
-#     news_title_format = "+".join(news_title_list)
-#     searched_news = search_news(news_title_format)
-#     title = f'search results for {news_title}'
-#     return render_template('search.html', news = searched_news)
+    if search_news:
+        return redirect(url_for('main.search', news_title=search_news))
+    else: 
+        return render_template('index.html', title= title, sports=sports, headlines= headlines, technology=technology, general=general, beauty=beauty, entertainment= entertainment, trending=trending, sources=sources)
+
+@main.route('/search/<news_title>')
+def search(news_title):
+    '''
+    View function to display the search results
+    '''
+    news_title_list = news_title.split(" ")
+    news_title_format = "+".join(news_title_list)
+    searched_news = search_news(news_title_format)
+    title = f'search results for {news_title}'
+    message = 'Hello World'
+    return render_template('search.html', news = searched_news, message=message)
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export NEWS_API_KEY='4a1c330567784e1693df798827673736'
+export NEWS_API_KEY='5ce38f880c884e2488e2cd2e3419c108'
 export SECRET_KEY='<Flask WTF Secret Key>'
 
 python3.9 manage.py server
